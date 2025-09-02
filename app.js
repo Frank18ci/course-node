f80e5b0874403beecb6323dad353bd2f7bba233b
Frank <fcarpio113@gmail.com>
2025-09-02 15:56:48 -0500

    Refactor error handling in server response logic

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ const showMessage = (message) => {
 }
 
 http.createServer((req, res) => {
-    if(req.url === "/" && req.method === "GET") {
+    try{
+        if(req.url === "/" && req.method === "GET") {
+        res.statusCode = 200
         res.end(JSON.stringify({"message": "Accediendo al servidor"}))
+    } else if(req.url === "/home" && req.method === "GET") {
+        res.statusCode = 300
+        res.end(JSON.stringify({"message": "Accediendo al servidor home"}))
+    } else {
+        res.statusCode = 404
+        res.end(JSON.stringify({"message": "Recurso no encontrado"}))
+    }
+    } catch(error) {
+        res.statusCode = 500
+        res.end(JSON.stringify({"message": "Error interno del servidor"}))
     }
 }).listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`)

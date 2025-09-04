abcce70ebb68983f56250a26fc5c65daa0b5532c
Frank <fcarpio113@gmail.com>
2025-09-04 15:39:34 -0500

    Add user retrieval functionality with getUser endpoint

--- express/controller/userController.js
+++ express/controller/userController.js
@@ -1,3 +1,7 @@
 exports.getAllUsers = (req, res) => {
     res.status(200).json({message: "List of all users"})
 }
+
+exports.getUser = (req, res) => {
+    res.status(200).json({message: `Details of user with ID ${req.params.id} is ${req.query.enable === 'true' ? 'true' : 'false'}`})
+}
--- express/routes/userRoute.js
+++ express/routes/userRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const userController = require('../controller/userController')
 
 router.get('/', userController.getAllUsers)
+router.get('/:id', userController.getUser)
 
 module.exports = router

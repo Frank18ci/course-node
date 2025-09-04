6b530eb7d5aebcbd16513216d816deb9a2d96d8c
Frank <fcarpio113@gmail.com>
2025-09-04 16:42:18 -0500

    Set up EJS view engine and create index view; update package.json to include EJS dependency

--- express/app.js
+++ express/app.js
@@ -3,17 +3,26 @@ const app = express()
 
 const morgan = require('morgan')
 
+const path = require('path')
+
 const userRouter = require('./routes/userRoute')
 
 //const userLogged = require('./middlewares/userLogged')
 
 //app.use(userLogged)
-
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-    res.status(200).json({"message": "Accediendo al servidor"})
+    const data = {
+        "title": "Home Page",
+        "message": "Welcome to the home page",
+        "showMessage": false,
+        "items": [1, 2, 3, 4, 5]
+    }
+    res.render('index', data)
 })
 app.use('/users', userRouter)
 

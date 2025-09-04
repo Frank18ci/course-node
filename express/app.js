0ce437e002ef29f8fc6101d9b8308d3d0219f782
Frank <fcarpio113@gmail.com>
2025-09-04 15:35:30 -0500

    Add user route and controller for fetching all users; update package.json for correct start script

--- express/app.js
+++ express/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const userRouter = require('./routes/userRoute')
 
 app.get('/', (req, res) => {
     res.status(200).json({"message": "Accediendo al servidor"})
 })
+app.use('/users', userRouter)
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
